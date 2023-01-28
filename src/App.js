79d3ee6271107c05132dedd90a908f84c143d687
ishrat <ishrat.lallmamode@gmail.com>
2023-01-28 14:39:40 -0700

    added tess

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import Tesseract from 'tesseract.js';
+
+let completeText; 
+
+function Text() {
+
+  
+
+}
+
+
+
+function getText() {
+
+  Tesseract.recognize(
+    'https://tesseract.projectnaptha.com/img/eng_bw.png',
+    'eng',
+    { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => {
+    completeText = text;
+    console.log(completeText);
+    return(<div>{completeText}</div>);
+  })
+
+  return(<div>{completeText}</div>);
+
+}
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return(<div>
+    <h1>Hello!</h1>
+    <img src="https://tesseract.projectnaptha.com/img/eng_bw.png"></img>
+    <Text />
+  </div>)
 }
 
 export default App;
